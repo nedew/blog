78751d2fb92bb8a417a270a52a0fb6c9c248a5dd
nedew <nedew.sn@gmail.com>
2020-10-19 02:14:34 +0900

    ユーザ名やプロフィールURLをblog.config.jsonで管理するように修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,14 @@
-// import Link from 'next/link'
 import styles from './styles/footer.module.scss'
+import { name, profUrl } from '../config/blog.config.json'
 
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
-        <div className={styles.copyright}>© 2020 Created by <a href='https://www.nedew.net' className={styles.me}>nedew</a></div>
+        <div className={styles.copyright}>
+          © 2020 Created by <a href={profUrl} className={styles.me}>{name}</a>
+        </div>
       </div>
     </footer>
   )
