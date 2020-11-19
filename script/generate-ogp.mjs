4ee35f9239574cc41cb4a403adabdf3b37a3a971
nedew <nedew.sn@gmail.com>
2020-11-20 02:39:07 +0900

    header色・contents areaのborder-radiusの変更

--- script/generate-ogp.mjs
+++ script/generate-ogp.mjs
@@ -2,34 +2,59 @@ import canvas from 'canvas'
 import fs from 'fs'
 import path from 'path'
 
-export function generateOgp(title, slug) {
+export function getLines(title, ctx) {
+  const TEXT_WIDTH = 900 // test
+  let lines = []
+  let currentLine = []
+  let tokens = Array.from(title)
+  let token
+  while ((token = tokens.shift())) {
+    const lineText = [...currentLine, token].join('')
+    if (ctx.measureText(lineText + token).width > TEXT_WIDTH) {
+      lines.push(currentLine.slice())
+      currentLine = [token]
+    } else {
+      currentLine.push(token)
+    }
+  }
+  if (currentLine.length) {
+    lines.push(currentLine)
+  }
+  console.log(lines)
+  return lines
+}
+
+export function generateOgpImg(title, slug) {
   // const OUTPUT_PATH = '../gen/ogp/'
 
   const WIDTH = 1200
   const HEIGHT = 630
-  const FONT_SIZE = 60
+  const LINE_HEIGHT = 60
   const FONT_FAMILY = '"Noto Sans CJK JP"'
   const FONT_TYPE = 'bold'
-  // const FONT = 'bold 85px "Noto Sans CJK JP"'
-  const LINE_HEIGHT = 100
+  const TEXT_WIDTH = 800
+  // const LINE_HEIGHT = 100
   // const FONT_COLOR = '#262626'
   const FONT_COLOR = 'white'
 
-  const cvs = canvas.createCanvas(WIDTH, HEIGHT)
-  const ctx = cvs.getContext('2d')
+  const canv = canvas.createCanvas(WIDTH, HEIGHT)
+  const ctx = canv.getContext('2d')
 
-  ctx.font = FONT_TYPE + ' ' + FONT_SIZE + 'px ' + FONT_FAMILY
+  ctx.font = FONT_TYPE + ' ' + LINE_HEIGHT + 'px ' + FONT_FAMILY
   ctx.fillStyle = FONT_COLOR
 
-  const textWidth = ctx.measureText(title).width
+  // const titleLines = getLines('Next.js, MDX, Typescriptで自分専用のブログシステムを作った（これは穴埋めようのテキストです）', ctx)
+  const titleLines = getLines('Overwatch のプレイヤーデータを返す Discord Bot を作って公開してみた。（ discord.js , node.js , overwatch ）', ctx)
+  for (let i = 0; i < titleLines.length; i++) {
+    const text = titleLines[i].join('')
+    ctx.fillText(text, (WIDTH - ctx.measureText(text).width) / 2, LINE_HEIGHT + (LINE_HEIGHT * i) + (5 * i))
+  }
 
-  ctx.fillText(title, (WIDTH - textWidth) / 2, FONT_SIZE)
+  // const textWidth = ctx.measureText(title).width
+  // ctx.fillText(title, (WIDTH - textWidth) / 2, LINE_HEIGHT)
+  const buf = canv.toBuffer()
 
-  const buf = cvs.toBuffer()
-
-  // const fileName = OUTPUT_PATH + './test.jpg'
-  const fileName = './test.jpg'
   fs.writeFileSync(path.join(process.cwd(), `gen/ogp/${slug}.png`), buf)
 }
 
-generateOgp('Next.js, MDXで自分用のブログを作成した', '5428374823')
+generateOgpImg('Next.js, MDXで自分用のブログを作成した', '5428374823')
