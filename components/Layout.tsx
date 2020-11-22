31647181e308f76f6289e1185ae58e7ea34eaa65
nedew <nedew.sn@gmail.com>
2020-11-22 17:07:18 +0900

    icon等の追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,13 +13,18 @@ export default function Layout({
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        {/* <meta
           name="description"
           content="My Blog"
-        />
+        /> */}
         <meta property="og:site_title" content={siteName} />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:image" content="/icon-512x512.png" />
         <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
       </Head>
       <div className={styles.area}>
         <Header />
