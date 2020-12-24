edd0d878cbcc58d0886fb1958dc9062aca27f4ef
nedew <nedew.sn@gmail.com>
2020-12-24 14:36:49 +0900

    Footerの編集

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import styles from './styles/footer.module.scss'
-import { name, profUrl } from '../config/blog.config.json'
+import { name, profUrl, siteName } from '../config/blog.config.json'
 
 
 export default function Footer() {
@@ -8,7 +8,17 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <div className={styles.copyright}>
-          <span>© 2020 Made by <a href={profUrl} className={styles.me}>{name}</a></span>
+          {/* <span>© 2020 Made by <a href={profUrl} className={styles.me}>{name}</a></span> */}
+          © 2020 {siteName}
+        </div>
+        <div className={styles.footerLinks}>
+          <Link href='/articles'>
+            <a>ARTICLES</a>
+          </Link>
+          <Link href='/tags'>
+            <a>TAGS</a>
+          </Link>
+          <a href={profUrl}>ABOUTME</a>
         </div>
       </div>
     </footer>
