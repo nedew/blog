496accf6446eec657713cdb480c940d0ca2c681e
nedew <nedew.sn@gmail.com>
2020-12-29 09:16:45 +0900

    fontの調査

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&Orbitron:wght@900&Montserrat:wght@700&display=swap" rel="stylesheet" /> */}
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@900&display=swap" rel="stylesheet" />
