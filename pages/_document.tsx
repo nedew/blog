bba8489b022a0a0ec416a09d16834cefe67d0303
nedew <nedew.sn@gmail.com>
2020-12-29 09:19:28 +0900

    fontの調査2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&Orbitron:wght@900&Montserrat:wght@700&display=swap" rel="stylesheet" /> */}
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&Orbitron:wght@900&Montserrat:wght@700&display=swap" rel="stylesheet" />
+          {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@900&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" /> */}
         </Head>
         <body>
           <Main />
