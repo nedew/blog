fa71024988b447d4328d4209983866262fdbb1a2
nedew <nedew.sn@gmail.com>
2020-12-29 19:19:18 +0900

    不要なCSS・ファイルを削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { siteName } from '../config/blog.config.json'
 
 export default class MyDocument extends Document {
   render() {
@@ -7,11 +6,7 @@ export default class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&Orbitron:wght@900&Montserrat:wght@700&display=swap" rel="stylesheet" /> */}
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Orbitron:wght@900&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
-          {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@900&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet" /> */}
         </Head>
         <body>
           <Main />
